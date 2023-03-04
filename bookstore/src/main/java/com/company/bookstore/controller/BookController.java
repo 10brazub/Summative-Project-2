230fa841dfd3263c9f55683c7a72c065d78891d3
Brandon Zubia <76754843+10brazub@users.noreply.github.com>
2023-03-03 19:46:06 -0600

    added crud mappings to BookController

--- bookstore/src/main/java/com/company/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/company/bookstore/controller/BookController.java
@@ -2,22 +2,53 @@ package com.company.bookstore.controller;
 
 import com.company.bookstore.model.Book;
 import com.company.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
 
+    @Autowired
     BookRepository bookRepository;
 
     @PostMapping("/book")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void addBook(@RequestBody Book book) {
-
+    public Book addBook(@RequestBody Book book) {
+        return bookRepository.save(book);
     }
 
+    @GetMapping("/book/{id}")
+    public Book getBookById(@PathVariable int id) {
+        Optional<Book> returnVal = bookRepository.findById(id);
+
+        return returnVal.orElse(null);
+    }
+
+    @GetMapping("/book")
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PutMapping("/book")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void updateBook(@RequestBody Book book) {
+        bookRepository.save(book);
+    }
+
+    @DeleteMapping("/book")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteBook(@RequestBody Book book) {
+        bookRepository.delete(book);
+    }
+
+    @GetMapping("/book/author/{id}")
+    public Book getBookByAuthorId(@PathVariable int id) {
+        return bookRepository.findByAuthorId(id);
+
+    }
 
 }
