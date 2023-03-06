2e774e04596a006a78fbb9402ad06e37ca9bb600
Brandon Zubia <76754843+10brazub@users.noreply.github.com>
2023-03-06 14:11:59 -0600

    Added crud operations for Author and Publisher

--- bookstore/src/main/java/com/company/bookstore/controller/AuthorController.java
+++ bookstore/src/main/java/com/company/bookstore/controller/AuthorController.java
@@ -1,4 +1,50 @@
 package com.company.bookstore.controller;
 
+import com.company.bookstore.model.Author;
+import com.company.bookstore.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class AuthorController {
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @PostMapping("/author")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Author addAuthor(@RequestBody Author author) {
+        return authorRepository.save(author);
+    }
+
+    @GetMapping("/author/{id}")
+    public Author addAuthorById(@PathVariable int id) {
+        Optional<Author> returnVal = authorRepository.findById(id);
+
+        return returnVal.orElse(null);
+    }
+
+    @GetMapping("/author")
+    public List<Author> getAllAuthors() {
+
+        return authorRepository.findAll();
+    }
+
+    @PutMapping("/author")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void updateAuthor(@RequestBody Author author) {
+
+        authorRepository.save(author);
+    }
+
+    @DeleteMapping("/author")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteAuthor(@RequestBody Author author) {
+
+        authorRepository.delete(author);
+    }
+
 }
--- bookstore/src/main/java/com/company/bookstore/controller/PublisherController.java
+++ bookstore/src/main/java/com/company/bookstore/controller/PublisherController.java
@@ -1,4 +1,50 @@
 package com.company.bookstore.controller;
 
+import com.company.bookstore.model.Publisher;
+import com.company.bookstore.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class PublisherController {
+
+    @Autowired
+    PublisherRepository publisherRepository;
+
+    @PostMapping("/publisher")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Publisher addPublisher(@RequestBody Publisher publisher) {
+        return publisherRepository.save(publisher);
+    }
+
+    @GetMapping("/publisher/{id}")
+    public Publisher addPublisherById(@PathVariable int id) {
+        Optional<Publisher> returnVal = publisherRepository.findById(id);
+
+        return returnVal.orElse(null);
+    }
+
+    @GetMapping("/publisher")
+    public List<Publisher> getAllPublisher() {
+
+        return publisherRepository.findAll();
+    }
+
+    @PutMapping("/publisher")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void updatePublisher(@RequestBody Publisher publisher) {
+
+        publisherRepository.save(publisher);
+    }
+
+    @DeleteMapping("/publisher")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deletePublisher(@RequestBody Publisher publisher) {
+
+        publisherRepository.delete(publisher);
+    }
+
 }
