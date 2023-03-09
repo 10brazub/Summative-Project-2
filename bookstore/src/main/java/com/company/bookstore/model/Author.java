5c9113301bf3d517e0c5aa9ed00bc6f78e2edd5e
Robert Velasquez <rvela042@fiu.edu>
2023-03-08 20:17:40 -0500

    Deleted unnecessary get methods

--- bookstore/src/main/java/com/company/bookstore/model/Author.java
+++ bookstore/src/main/java/com/company/bookstore/model/Author.java
@@ -35,64 +35,32 @@ public class Author implements Serializable {
         this.authorId = authorId;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public String getStreet() {
-        return street;
-    }
-
     public void setStreet(String street) {
         this.street = street;
-    }
-
-    public String getCity() {
-        return city;
     }
 
     public void setCity(String city) {
         this.city = city;
     }
 
-    public String getState() {
-        return state;
-    }
-
     public void setState(String state) {
         this.state = state;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
     }
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
     public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
