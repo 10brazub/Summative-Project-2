2b8cb372261e28ac1c3d76e5fa3cf10fb0883aa5
Robert Velasquez <rvela042@fiu.edu>
2023-03-08 19:03:23 -0500

    Updated Author Controller

--- bookstore/src/main/java/com/company/bookstore/controller/AuthorController.java
+++ bookstore/src/main/java/com/company/bookstore/controller/AuthorController.java
@@ -1,7 +1,7 @@
 package com.company.bookstore.controller;
 
-import com.company.bookstore.model.Author;
-import com.company.bookstore.repository.AuthorRepository;
+import com.company.bookstore.model.Publisher;
+import com.company.bookstore.repository.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,39 +12,36 @@ import java.util.Optional;
 public class AuthorController {
 
     @Autowired
-    AuthorRepository authorRepository;
+    PublisherRepository repo;
 
-    @PostMapping("/author")
-    @ResponseStatus(value = HttpStatus.CREATED)
-    public Author addAuthor(@RequestBody Author author) {
-        return authorRepository.save(author);
+    @PostMapping("/publisher")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Publisher addPublisher(@RequestBody Publisher Publisher) {
+        return repo.save(Publisher);
     }
 
-    @GetMapping("/author/{id}")
-    public Author addAuthorById(@PathVariable int id) {
-        Optional<Author> returnVal = authorRepository.findById(id);
-
-        return returnVal.orElse(null);
+    @PutMapping("/publisher")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updatePublisher(@RequestBody Publisher Publisher) {
+        repo.save(Publisher);
     }
 
-    @GetMapping("/author")
-    public List<Author> getAllAuthors() {
+    @DeleteMapping("/publisher/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePublisher(@PathVariable int id) { repo.deleteById(id); }
 
-        return authorRepository.findAll();
+    @GetMapping("/publisher/{id}")
+    public Publisher getPublisherById(@PathVariable int id) {
+        Optional<Publisher> returnVal = repo.findById(id);
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
     }
 
-    @PutMapping("/author")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void updateAuthor(@RequestBody Author author) {
-
-        authorRepository.save(author);
+    @GetMapping("/publisher")
+    public List<Publisher> getAllPublishers() {
+        return repo.findAll();
     }
-
-    @DeleteMapping("/author")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void deleteAuthor(@RequestBody Author author) {
-
-        authorRepository.delete(author);
-    }
-
 }
