b4bd17000c9447e6b89edbd6177094a3371f93fa
Brandon Zubia <76754843+10brazub@users.noreply.github.com>
2023-03-10 20:58:52 -0600

    updated book repository test

--- bookstore/src/test/java/com/company/bookstore/repository/BookRepositoryTest.java
+++ bookstore/src/test/java/com/company/bookstore/repository/BookRepositoryTest.java
@@ -9,12 +9,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import java.util.List;
 import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookRepositoryTest {
 
@@ -30,6 +32,8 @@ public class BookRepositoryTest {
     @Before
     public void setUp() throws Exception {
         bookRepository.deleteAll();
+        authorRepository.deleteAll();
+        publisherRepository.deleteAll();
     }
 
     @Test
